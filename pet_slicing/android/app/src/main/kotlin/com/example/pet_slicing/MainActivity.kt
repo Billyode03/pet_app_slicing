f6f021ccd818140faa4943b62269391bb947985b
BillyOde03 <naubilode@gmail.com>
2024-06-25 16:44:59 +0800

    Start Slicing

---
+++ pet_slicing/android/app/src/main/kotlin/com/example/pet_slicing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pet_slicing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pet_slicing/lib/screens/home_screens.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreens extends StatelessWidget {
+  const HomeScreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
